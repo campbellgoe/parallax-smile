99bdbe2dcaadd42bec6ea4eddec50381e2d5f555
George Campbell <campbell.goe@gmail.com>
2026-08-18 17:32:05 +0100

    repel and attract

--- emotions/interactions.ts
+++ emotions/interactions.ts
@@ -8,7 +8,7 @@ export function interact(
 ) {
   const dominant =
     getDominantEmotion(source);
-  
+
   const influences =
     influenceMatrix[dominant];
 
@@ -27,34 +27,43 @@ export function interact(
   }
   const targetDominant = getDominantEmotion(target)
   const px = target.x;
-const py = target.y;
+  const py = target.y;
 
-const dx = source.x - px;
-const dy = source.y - py;
+  const dx = source.x - px;
+  const dy = source.y - py;
 
-const distBetween = Math.hypot(dx, dy);
+  const distBetween = Math.hypot(dx, dy);
 
-if (distBetween < 500 && distBetween >10) {
-  const directionX = dx / distBetween;
-  const directionY = dy / distBetween;
-  if(distBetween < 300 && dominant === 'fear') {
-    if(targetDominant === 'anger' || targetDominant === 'fire'){
-      source.x += directionX * 0.25;
-      source.y += directionY * 0.25;
-    } else {
-      source.x -= directionX * 0.01;
-      source.y -= directionY * 0.01;
+  if (distBetween < 500 && distBetween > 10) {
+    const directionX = dx / distBetween;
+    const directionY = dy / distBetween;
+    if (dominant === 'fear') {
+      if (targetDominant === 'anger') {
+        source.x += directionX * 0.25;
+        source.y += directionY * 0.25;
+      } else if (targetDominant === 'fire') {
+        source.x += directionX;
+        source.y += directionY;
+      } else {
+        source.x -= directionX * 0.01;
+        source.y -= directionY * 0.01;
+      }
+    }
+    if (dominant === 'anger') {
+      if (targetDominant === 'fear') {
+        source.x -= directionX * 0.25;
+        source.y -= directionY * 0.25;
+      } else {
+        // source.x -= directionX * 0.01;
+        // source.y -= directionY * 0.01;
+      }
+    }
+    if(distBetween > 25 && dominant === 'joy' || dominant === 'love'){
+      if(targetDominant === 'joy' || targetDominant === 'love'){
+         source.x -= directionX * 0.1;
+         source.y -= directionY * 0.1;
+      }
     }
   }
-   if(distBetween < 500 && dominant === 'anger') {
-    if(targetDominant === 'fear'){
-      source.x -= directionX * 0.25;
-      source.y -= directionY * 0.25;
-    } else {
-      // source.x -= directionX * 0.01;
-      // source.y -= directionY * 0.01;
-    }
-  }
-}
-  
+
 }
