36ca49b978235f28adfec10da40ddb1e3a68766f
George Campbell <campbell.goe@gmail.com>
2026-08-18 20:03:05 +0100

    add utils

--- util/utils.ts
+++ util/utils.ts
@@ -1,17 +1,17 @@
-// import { clsx, type ClassValue } from "clsx"
-// import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-// export function cn(...inputs: ClassValue[]) {
-//   return twMerge(clsx(inputs))
-// }
-// export function slugify(str: string) {
-//   return str
-//     .toString()
-//     .toLowerCase()
-//     .trim() // Remove whitespace from both ends of a string
-//     .replace(/\s+/g, '-') // Replace spaces with -
-//     .replace(/&/g, '-and-') // Replace & with 'and'
-//     .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
-//     .replace(/\-\-+/g, '-') // Replace multiple - with single -
-// }
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+export function slugify(str: string) {
+  return str
+    .toString()
+    .toLowerCase()
+    .trim() // Remove whitespace from both ends of a string
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+}
 
